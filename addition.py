20c2371df538ae07682ea1115dd4290aa4313dad
SIRISHA R <160576579+SIRISHA-RAJ@users.noreply.github.com>
2025-04-25 18:38:23 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,13 @@
+# List of marks
+marks = [45, 67, 38, 50, 42]
+
+# Calculate average
+average = sum(marks) / len(marks)
+
+# Check pass or fail
+if average >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+    
